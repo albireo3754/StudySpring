71d96caf4ac3a0118a49f9e844469e1e67244c89
alibreo3754 <starpro123@naver.com>
2023-12-08 14:36:30 +0900

    Spring with JDBC Template

---
+++ relational-data-access/README.md
@@ -0,0 +1,6 @@
+# Accessing Relational Data using JDBC with Spring
+
+### https://spring.io/guides/gs/relational-data-access/#scratch
+
+## Learning
+- CommandLineRunner를 Application에 직접 implements도 가능함
---
+++ relational-data-access/src/test/java/com/albireo3754/relationaldataaccess/RelationalDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albireo3754.relationaldataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
